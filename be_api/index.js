42610b0562567823d8638d7b05b8a2ec7c71957e
Khoa <khoanguyen.1881@gmail.com>
2024-12-27 22:17:17 +0700

    update comment

--- be_api/index.js
+++ be_api/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//- cors se giup ket noi 2 server
 const cors = require("cors");
 app.use(cors());
 
+//-.env 
 require("dotenv").config();
 //-connect database
 const dataBase = require("./config/database");
@@ -12,8 +14,6 @@ dataBase.connect();
 
 //-connect route
 const routeAdmin = require("./routes/admin/index.route");
-
-
 
 routeAdmin(app);
 
