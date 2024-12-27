fc0d12ba59939278251899f96ea460ae33671410
Khoa <khoanguyen.1881@gmail.com>
2024-12-27 22:18:11 +0700

    Update database.js

--- be_api/config/database.js
+++ be_api/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 module.exports.connect = async () => {
   try {
-    // await mongoose.connect("mongodb://127.0.0.1:27017/product-management")
     await mongoose.connect(process.env.MONGO_URL)
     console.log('Kết nối thành công')
   } catch (error) {
